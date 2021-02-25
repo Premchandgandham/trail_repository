0b1c043e90f5c100202b093c0133c567dd6ced47
ros <g.premchand5030@gmail.com>
2021-02-25 17:19:22 +0530

    mapping

---
+++ gmapping2/build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/ros/catkin_ws/src/gmapping
+# Build directory: /home/ros/catkin_ws/src/gmapping/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")

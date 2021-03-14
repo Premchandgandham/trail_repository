04267db3980c10a41ffadc06a80b2abb14bbee30
Premchandgandham <76577102+Premchandgandham@users.noreply.github.com>
2021-03-15 03:28:06 +0530

    Delete rtab/build directory

--- rtab/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ros/catkin_ws/src/rtab
-# Build directory: /home/ros/catkin_ws/src/rtab/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
